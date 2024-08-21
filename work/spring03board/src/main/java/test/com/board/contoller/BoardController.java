9bc6b049a98b0b5d18a408879969e159afb3596e
Lee Sumin <bluky0516@gmail.com>
2024-08-21 17:04:53 +0900

    add model and upgrade view

--- work/spring03board/src/main/java/test/com/board/contoller/BoardController.java
+++ work/spring03board/src/main/java/test/com/board/contoller/BoardController.java
@@ -2,20 +2,21 @@ package test.com.board.contoller;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import lombok.extern.slf4j.Slf4j;
 import test.com.board.model.BoardVO;
 import test.com.board.service.BoardService;
 
+@Slf4j
 @Controller
 public class BoardController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
 	
 	@Autowired
 	BoardService service;
@@ -23,20 +24,20 @@ public class BoardController {
 	
 	@RequestMapping(value = "/b_insert.do", method = RequestMethod.GET)
 	public String b_insert() {
-		logger.info("b_insert");
+		log.info("b_insert");
 		
 		return "board/insert";
 	}
 	
 	@RequestMapping(value = "/b_update.do", method = RequestMethod.GET)
-	public String b_update() {
-		logger.info("b_update");
-		
-		BoardVO vo = new BoardVO();
-		vo.setNum(11);
+	public String b_update(BoardVO vo,Model model) {
+		log.info("b_update");
+		log.info("vo:{}",vo);
 		
 		BoardVO vo2 = service.selectOne(vo);
-		logger.info("vo2:{}",vo2);
+		log.info("vo2:{}",vo2);
+		
+		model.addAttribute("vo2",vo2);
 		
 		return "board/update";
 	}
@@ -44,94 +45,83 @@ public class BoardController {
 	
 	@RequestMapping(value = "/b_delete.do", method = RequestMethod.GET)
 	public String b_delete() {
-		logger.info("b_delete");
+		log.info("b_delete");
 		
 		return "board/delete";
 	}
 	
 	
 	@RequestMapping(value = "/b_selectOne.do", method = RequestMethod.GET)
-	public String b_selectOne() {
-		logger.info("b_selectOne");
-		
-		BoardVO vo = new BoardVO();
-		vo.setNum(11);
+	public String b_selectOne(BoardVO vo,Model model) {
+		log.info("b_selectOne");
+		log.info("vo:{}",vo);
 		
 		BoardVO vo2 = service.selectOne(vo);
-		logger.info("vo2:{}",vo2);
+		log.info("vo2:{}",vo2);
+		
+		model.addAttribute("vo2",vo2);
 		
 		return "board/selectOne";
 	}
 	
 	@RequestMapping(value = "/b_selectAll.do", method = RequestMethod.GET)
-	public String b_selectAll() {
-		logger.info("b_selectAll");
+	public String b_selectAll(Model model) {
+		log.info("b_selectAll");
 		
 		List<BoardVO> list = service.selectAll();
-		logger.info("{}",list);
+		log.info("{}",list);
 		
+		model.addAttribute("list",list);
 		
 		return "board/selectAll";
 	}
 	
 	@RequestMapping(value = "/b_searchList.do", method = RequestMethod.GET)
-	public String b_searchList() {
-		logger.info("b_searchList");
-		
-		String searchKey = "title";
-		String searchWord = "ja";
+	public String b_searchList(Model model,@RequestParam(defaultValue = "title")String searchKey,
+			@RequestParam(defaultValue = "sp")String searchWord) {
+		log.info("b_searchList");
+		log.info("searchKey:{}",searchKey);
+		log.info("searchWord:{}",searchWord);
 		
 		List<BoardVO> list = service.searchList(searchKey,searchWord);
-		logger.info("{}",list);
+		log.info("{}",list);
+		
+		model.addAttribute("list",list);
 		
 		
 		return "board/selectAll";
 	}
 	
 	
-	@RequestMapping(value = "/b_insertOK.do", method = RequestMethod.GET)
-	public String b_insertOK() {
-		logger.info("b_insertOK");
-		
-		BoardVO vo = new BoardVO();
-		//vo.setNum(1);
-		vo.setTitle("spring");
-		vo.setContent("hello");
-		vo.setWriter("admin");
-		vo.setWdate("2024-12-25");
+	@RequestMapping(value = "/b_insertOK.do", method = RequestMethod.POST)
+	public String b_insertOK(BoardVO vo) {
+		log.info("b_insertOK");
+		log.info("vo:{}",vo);
 		
 		int result = service.insert(vo);
-		logger.info("result:{}",result);
+		log.info("result:{}",result);
 		
 		return "redirect:b_selectAll.do";
 	}
 		
-	@RequestMapping(value = "/b_updateOK.do", method = RequestMethod.GET)
-	public String b_updateOK() {
-		logger.info("b_updateOK");
-		
-		BoardVO vo = new BoardVO();
-		vo.setNum(1);
-		vo.setTitle("spring");
-		vo.setContent("hello");
-		vo.setWriter("admin");
-		vo.setWdate("2024-12-25");
+	@RequestMapping(value = "/b_updateOK.do", method = RequestMethod.POST)
+	public String b_updateOK(BoardVO vo) {
+		log.info("b_updateOK");
+		log.info("vo:{}",vo);
 		
 		int result = service.update(vo);
-		logger.info("result:{}",result);
+		log.info("result:{}",result);
 		
 		return "redirect:b_selectOne.do?num=33";
 	}
 	
-	@RequestMapping(value = "/b_deleteOK.do", method = RequestMethod.GET)
-	public String b_deleteOK() {
-		logger.info("b_deleteOK");
-		
-		BoardVO vo = new BoardVO();
-		vo.setNum(1);
+	@RequestMapping(value = "/b_deleteOK.do", method = RequestMethod.POST)
+	public String b_deleteOK(BoardVO vo) {
+		log.info("b_deleteOK");
+		log.info("vo:{}",vo);
 		
 		int result = service.delete(vo);
-		logger.info("result:{}",result);
+		log.info("result:{}",result);
 		
 		return "redirect:b_selectAll.do";
 	}
