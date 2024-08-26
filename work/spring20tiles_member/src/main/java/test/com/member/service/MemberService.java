542c8fd974799fc897607511e68cce89e6589bbc
Lee Sumin <bluky0516@gmail.com>
2024-08-26 17:27:10 +0900

    add tiles on member

---
+++ work/spring20tiles_member/src/main/java/test/com/member/service/MemberService.java
@@ -0,0 +1,18 @@
+package test.com.member.service;
+
+import java.io.IOException;
+import java.util.List;
+
+import test.com.member.model.MemberVO;
+
+public interface MemberService {
+	
+	public int insert(MemberVO vo) throws IllegalStateException, IOException;
+	public int update(MemberVO vo) throws IllegalStateException, IOException;
+	public int delete(MemberVO vo);
+	public MemberVO selectOne(MemberVO vo);
+	public List<MemberVO> selectAll();
+	public List<MemberVO> searchList(String searchKey,String searchWord);
+	public MemberVO idCheck(MemberVO vo);
+
+}
---
+++ work/spring20tiles_member/src/main/webapp/WEB-INF/layouts/header.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<ul id="menu">
+	<li>
+		<a href="home.do">HOME</a>
+	</li>
+	<li>
+		<a href="m_insert.do">MEMBER</a>
+		<ul>
+			<li><a href="m_insert.do">m_insert.do</a></li>
+			<li><a href="m_selectAll.do">m_selectAll.do</a></li>
+		</ul>
+	</li>
+</ul>
+<hr>
